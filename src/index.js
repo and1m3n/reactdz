560322fd9bca75a4d12e5a5cf659f5645167a455
and1m3n <81037448+and1m3n@users.noreply.github.com>
2021-10-16 19:13:32 +0300

    dz1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Message(props) {
+  return <h1 className='lala'>Привет, {props.name}</h1>;
+}
+
+const element = <Message name="REACT" />;
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
+function Apps() {
+  return (
+    <div>
+      <Message name="JS" />
+      <Message name="JSX" />
+      <Message name="ReactJs" />
+    </div>
+  );
 
+}
+
+ReactDOM.render(
+  <Apps />,
+  document.getElementById('root')
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
